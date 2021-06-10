d8484d25c1a88a0885f9c752cd0a20451fc83ee0
2020snow <2103200855@qq.com>
2021-06-10 11:47:55 +0800

    ‘up’

---
+++ gallery/js/gallery.js
@@ -0,0 +1,62 @@
+/**
+ *
+ *
+ */
+function initGalleryByIndex(index) {
+    const divs = ["gallery-one", "gallery-two", "gallery-three"];
+    var gallery_two = document.getElementById(divs[index]);
+
+    // 每次初始化时判断是否已经有子元素，如果有，则清空子元素
+    if (gallery_two && gallery_two.hasChildNodes()) {
+        var element_divs = document.getElementsByName("gallery-grid-item-" + index);
+        if (element_divs && element_divs.length > 0) {
+            for (var i = element_divs.length - 1; i >= 0; i--) {
+                gallery_two.removeChild(element_divs[i]);
+            }
+        }
+
+    }
+
+    var gallerys = gallery_pic[index];
+    for (var i = 0; i< gallerys.length; i++) {
+        var grid_item_div = document.createElement("div");
+        grid_item_div.setAttribute("class", "grid-item");
+        grid_item_div.setAttribute("name", "gallery-grid-item-" + index);
+
+        var figure = document.createElement("figure");
+        figure.setAttribute("class", "effect-sadie");
+
+        var img = document.createElement("img");
+        img.setAttribute("src", gallerys[i].src == "" ? gallerys[i].href : gallerys[i].src);
+        img.setAttribute("alt", gallerys[i].alt);
+        img.setAttribute("class", "img-fluid tm-img");
+
+        var figcaption = document.createElement("figcaption");
+
+        var h2 = document.createElement("h2");
+        h2.setAttribute("class", "tm-figure-title");
+        var tm_figure_title = gallerys[i].tm_figure_title == "" ? "###" : gallerys[i].tm_figure_title;
+        h2.innerHTML = "<span><strong>" + tm_figure_title + "</strong></span>";
+
+        var p = document.createElement("p");
+        p.setAttribute("class", "tm-figure-description");
+        p.innerText = gallerys[i].tm_figure_description == "" ? "###" : gallerys[i].tm_figure_description;
+
+        var a = document.createElement("a");
+        a.setAttribute("href", gallerys[i].href);
+        a.innerText = gallerys[i].text;
+
+        figcaption.appendChild(h2);
+        figcaption.appendChild(p);
+        figcaption.appendChild(a);
+
+        figure.appendChild(img);
+        figure.appendChild(figcaption);
+
+        grid_item_div.appendChild(figure);
+        gallery_two.appendChild(grid_item_div);
+
+    }
+
+}
+
---
+++ gallery/js/gallery_pic.js
@@ -0,0 +1,298 @@
+gallery_pic = [
+    [{'src': 'img/1/0212c228207c11da4841ec9105fdf3b4-tuya.jpg', 'alt': 'Image', 'tm_figure_title': '', 'tm_figure_description': '', 'href': 'img/1/0212c228207c11da4841ec9105fdf3b4-tuya.jpg', 'text': 'View More'}, {'src': 'img/1/298abc77e53a7094c3530d3fcb7e7394-tuya.jpg', 'alt': 'Image', 'tm_figure_title': '', 'tm_figure_description': '', 'href': 'img/1/298abc77e53a7094c3530d3fcb7e7394-tuya.jpg', 'text': 'View More'}, {'src': 'img/1/2c12b0083433f20b0319cddb537f46fa-tuya.jpg', 'alt': 'Image', 'tm_figure_title': '', 'tm_figure_description': '', 'href': 'img/1/2c12b0083433f20b0319cddb537f46fa-tuya.jpg', 'text': 'View More'}, {'src': 'img/1/35ec433fa19e20ea75102cc9d98e06d9-tuya.jpg', 'alt': 'Image', 'tm_figure_title': '', 'tm_figure_description': '', 'href': 'img/1/35ec433fa19e20ea75102cc9d98e06d9-tuya.jpg', 'text': 'View More'}, {'src': 'img/1/3c541a5a300dfcc70837d98bc3c2a28f-tuya.jpg', 'alt': 'Image', 'tm_figure_title': '', 'tm_figure_description': '', 'href': 'img/1/3c541a5a300dfcc70837d98bc3c2a28f-tuya.jpg', 'text': 'View More'}, {'src': 'img/1/3d216e5dd71a5654b991935f775929ed-tuya.jpg', 'alt': 'Image', 'tm_figure_title': '', 'tm_figure_description': '', 'href': 'img/1/3d216e5dd71a5654b991935f775929ed-tuya.jpg', 'text': 'View More'}, {'src': 'img/1/42c7521d200618e3bf6a01728e9f95a0-tuya.jpg', 'alt': 'Image', 'tm_figure_title': '', 'tm_figure_description': '', 'href': 'img/1/42c7521d200618e3bf6a01728e9f95a0-tuya.jpg', 'text': 'View More'}, {'src': 'img/1/4af1d44aa5d38bca9b48344fdd6a157f-tuya.jpg', 'alt': 'Image', 'tm_figure_title': '', 'tm_figure_description': '', 'href': 'img/1/4af1d44aa5d38bca9b48344fdd6a157f-tuya.jpg', 'text': 'View More'}, {'src': 'img/1/55a67e703b9d4c0e07f59329a2d822da-tuya.jpg', 'alt': 'Image', 'tm_figure_title': '', 'tm_figure_description': '', 'href': 'img/1/55a67e703b9d4c0e07f59329a2d822da-tuya.jpg', 'text': 'View More'}, {'src': 'img/1/95a62f93cc9f3161ed52813c92148c54-tuya.jpg', 'alt': 'Image', 'tm_figure_title': '', 'tm_figure_description': '', 'href': 'img/1/95a62f93cc9f3161ed52813c92148c54-tuya.jpg', 'text': 'View More'}, {'src': 'img/1/98deafc525fbd910932067aabf8e68e7-tuya.jpg', 'alt': 'Image', 'tm_figure_title': '', 'tm_figure_description': '', 'href': 'img/1/98deafc525fbd910932067aabf8e68e7-tuya.jpg', 'text': 'View More'}, {'src': 'img/1/99ed2ae9de32e8cd6b7c9af14425a283-tuya.jpg', 'alt': 'Image', 'tm_figure_title': '', 'tm_figure_description': '', 'href': 'img/1/99ed2ae9de32e8cd6b7c9af14425a283-tuya.jpg', 'text': 'View More'}, {'src': 'img/1/a49b5c7a78fac3cf25eb33992ee6bb01-tuya.jpg', 'alt': 'Image', 'tm_figure_title': '', 'tm_figure_description': '', 'href': 'img/1/a49b5c7a78fac3cf25eb33992ee6bb01-tuya.jpg', 'text': 'View More'}, {'src': 'img/1/a4acb605802d52a4051667d32e755d5a-tuya.jpg', 'alt': 'Image', 'tm_figure_title': '', 'tm_figure_description': '', 'href': 'img/1/a4acb605802d52a4051667d32e755d5a-tuya.jpg', 'text': 'View More'}, {'src': 'img/1/bdf010a61ba6dc385b662c7eadcffdc8-tuya.jpg', 'alt': 'Image', 'tm_figure_title': '', 'tm_figure_description': '', 'href': 'img/1/bdf010a61ba6dc385b662c7eadcffdc8-tuya.jpg', 'text': 'View More'}, {'src': 'img/1/c030ee9e04d784d54a2f66a67d8d6fdc-tuya.jpg', 'alt': 'Image', 'tm_figure_title': '', 'tm_figure_description': '', 'href': 'img/1/c030ee9e04d784d54a2f66a67d8d6fdc-tuya.jpg', 'text': 'View More'}, {'src': 'img/1/d69c390c49e848f13ba4201d4f59aa6f-tuya.jpg', 'alt': 'Image', 'tm_figure_title': '', 'tm_figure_description': '', 'href': 'img/1/d69c390c49e848f13ba4201d4f59aa6f-tuya.jpg', 'text': 'View More'}, {'src': 'img/1/e7d0485fc328fdae2b48e13808acc16b-tuya.jpg', 'alt': 'Image', 'tm_figure_title': '', 'tm_figure_description': '', 'href': 'img/1/e7d0485fc328fdae2b48e13808acc16b-tuya.jpg', 'text': 'View More'}, {'src': 'img/1/f84f8ffb44ef571ce54d8c0dc297e9fd-tuya.jpg', 'alt': 'Image', 'tm_figure_title': '', 'tm_figure_description': '', 'href': 'img/1/f84f8ffb44ef571ce54d8c0dc297e9fd-tuya.jpg', 'text': 'View More'}, {'src': 'img/1/IMG_2162-tuya.jpg', 'alt': 'Image', 'tm_figure_title': '', 'tm_figure_description': '', 'href': 'img/1/IMG_2162-tuya.jpg', 'text': 'View More'}], // gallery one
+    [{'src': 'img/2/08a074dfc030eee563001044ef0a0dd4.jpg', 'alt': 'Image', 'tm_figure_title': '', 'tm_figure_description': '', 'href': 'img/2/08a074dfc030eee563001044ef0a0dd4.jpg', 'text': 'View More'}, {'src': 'img/2/0f789e12f01fd0347e4b13e468426c0a.jpg', 'alt': 'Image', 'tm_figure_title': '', 'tm_figure_description': '', 'href': 'img/2/0f789e12f01fd0347e4b13e468426c0a.jpg', 'text': 'View More'}, {'src': 'img/2/3ec1ffe0dcfbf69715f475d89408de22.jpg', 'alt': 'Image', 'tm_figure_title': '', 'tm_figure_description': '', 'href': 'img/2/3ec1ffe0dcfbf69715f475d89408de22.jpg', 'text': 'View More'}, {'src': 'img/2/3efb90d357c0075f758c99702e8e59f8.jpg', 'alt': 'Image', 'tm_figure_title': '', 'tm_figure_description': '', 'href': 'img/2/3efb90d357c0075f758c99702e8e59f8.jpg', 'text': 'View More'}, {'src': 'img/2/54cd63a1e3fa4af29a2c3276f0d2d8a2.jpg', 'alt': 'Image', 'tm_figure_title': '', 'tm_figure_description': '', 'href': 'img/2/54cd63a1e3fa4af29a2c3276f0d2d8a2.jpg', 'text': 'View More'}, {'src': 'img/2/63e697737c338c49031d4beb53bdfe6f.jpg', 'alt': 'Image', 'tm_figure_title': '', 'tm_figure_description': '', 'href': 'img/2/63e697737c338c49031d4beb53bdfe6f.jpg', 'text': 'View More'}, {'src': 'img/2/6a00752c365c842d177d2dca0c0427c7.jpg', 'alt': 'Image', 'tm_figure_title': '', 'tm_figure_description': '', 'href': 'img/2/6a00752c365c842d177d2dca0c0427c7.jpg', 'text': 'View More'}, {'src': 'img/2/706c0d8b72bb4efc367df94e2a550694.jpg', 'alt': 'Image', 'tm_figure_title': '', 'tm_figure_description': '', 'href': 'img/2/706c0d8b72bb4efc367df94e2a550694.jpg', 'text': 'View More'}, {'src': 'img/2/77bca51af91cf89e0d7eaae8a0f0efec.jpg', 'alt': 'Image', 'tm_figure_title': '', 'tm_figure_description': '', 'href': 'img/2/77bca51af91cf89e0d7eaae8a0f0efec.jpg', 'text': 'View More'}, {'src': 'img/2/8b5dfc1e60945ab1417909eb80459720.jpg', 'alt': 'Image', 'tm_figure_title': '', 'tm_figure_description': '', 'href': 'img/2/8b5dfc1e60945ab1417909eb80459720.jpg', 'text': 'View More'}, {'src': 'img/2/b4d04466a05c458c2ba3340df3d2521b.jpg', 'alt': 'Image', 'tm_figure_title': '', 'tm_figure_description': '', 'href': 'img/2/b4d04466a05c458c2ba3340df3d2521b.jpg', 'text': 'View More'}, {'src': 'img/2/c00cf6687a40f6a6aeb2b9cef4728831.jpg', 'alt': 'Image', 'tm_figure_title': '', 'tm_figure_description': '', 'href': 'img/2/c00cf6687a40f6a6aeb2b9cef4728831.jpg', 'text': 'View More'}, {'src': 'img/2/c75a893a6edcacd074594bef4bb2ad2c.jpg', 'alt': 'Image', 'tm_figure_title': '', 'tm_figure_description': '', 'href': 'img/2/c75a893a6edcacd074594bef4bb2ad2c.jpg', 'text': 'View More'}, {'src': 'img/2/d0740aa7b2efeb261cc33988384d3bba.jpg', 'alt': 'Image', 'tm_figure_title': '', 'tm_figure_description': '', 'href': 'img/2/d0740aa7b2efeb261cc33988384d3bba.jpg', 'text': 'View More'}, {'src': 'img/2/e38f9d7a17152bb98ad6c3e934eef660.jpg', 'alt': 'Image', 'tm_figure_title': '', 'tm_figure_description': '', 'href': 'img/2/e38f9d7a17152bb98ad6c3e934eef660.jpg', 'text': 'View More'}, {'src': 'img/2/f0f85cd942773b5ce767f4b037983855.jpg', 'alt': 'Image', 'tm_figure_title': '', 'tm_figure_description': '', 'href': 'img/2/f0f85cd942773b5ce767f4b037983855.jpg', 'text': 'View More'}],
+    [
+        {
+            "src": "img/3/51.jpg",
+            "alt": "Image",
+            "tm_figure_title": "",
+            "tm_figure_description": "",
+            "href": "img/3/51.jpg",
+            "text": "View More"
+        },
+        {
+            "src": "img/3/52.jpg",
+            "alt": "Image",
+            "tm_figure_title": "",
+            "tm_figure_description": "",
+            "href": "img/3/52.jpg",
+            "text": "View More"
+        },
+        {
+            "src": "img/3/53.jpg",
+            "alt": "Image",
+            "tm_figure_title": "",
+            "tm_figure_description": "",
+            "href": "img/3/53.jpg",
+            "text": "View More"
+        },
+        {
+            "src": "img/3/54.jpg",
+            "alt": "Image",
+            "tm_figure_title": "",
+            "tm_figure_description": "",
+            "href": "img/3/54.jpg",
+            "text": "View More"
+        },
+        {
+            "src": "img/3/55.jpg",
+            "alt": "Image",
+            "tm_figure_title": "",
+            "tm_figure_description": "",
+            "href": "img/3/55.jpg",
+            "text": "View More"
+        },
+        {
+            "src": "img/3/56.jpg",
+            "alt": "Image",
+            "tm_figure_title": "",
+            "tm_figure_description": "",
+            "href": "img/3/56.jpg",
+            "text": "View More"
+        },
+        {
+            "src": "img/3/57.jpg",
+            "alt": "Image",
+            "tm_figure_title": "",
+            "tm_figure_description": "",
+            "href": "img/3/57.jpg",
+            "text": "View More"
+        },
+        {
+            "src": "img/3/58.jpg",
+            "alt": "Image",
+            "tm_figure_title": "",
+            "tm_figure_description": "",
+            "href": "img/3/58.jpg",
+            "text": "View More"
+        },
+        {
+            "src": "img/3/59.jpg",
+            "alt": "Image",
+            "tm_figure_title": "",
+            "tm_figure_description": "",
+            "href": "img/3/59.jpg",
+            "text": "View More"
+        },
+        {
+            "src": "img/3/60.jpg",
+            "alt": "Image",
+            "tm_figure_title": "",
+            "tm_figure_description": "",
+            "href": "img/3/60.jpg",
+            "text": "View More"
+        },
+        {
+            "src": "img/3/61.jpg",
+            "alt": "Image",
+            "tm_figure_title": "",
+            "tm_figure_description": "",
+            "href": "img/3/61.jpg",
+            "text": "View More"
+        },
+        {
+            "src": "img/3/62.jpg",
+            "alt": "Image",
+            "tm_figure_title": "",
+            "tm_figure_description": "",
+            "href": "img/3/62.jpg",
+            "text": "View More"
+        },
+        {
+            "src": "img/3/63.jpg",
+            "alt": "Image",
+            "tm_figure_title": "",
+            "tm_figure_description": "",
+            "href": "img/3/63.jpg",
+            "text": "View More"
+        },
+        {
+            "src": "img/3/64.jpg",
+            "alt": "Image",
+            "tm_figure_title": "",
+            "tm_figure_description": "",
+            "href": "img/3/64.jpg",
+            "text": "View More"
+        },
+        {
+            "src": "img/3/65.jpg",
+            "alt": "Image",
+            "tm_figure_title": "",
+            "tm_figure_description": "",
+            "href": "img/3/65.jpg",
+            "text": "View More"
+        },
+        {
+            "src": "img/3/66.jpg",
+            "alt": "Image",
+            "tm_figure_title": "",
+            "tm_figure_description": "",
+            "href": "img/3/66.jpg",
+            "text": "View More"
+        },
+        {
+            "src": "img/3/67.jpg",
+            "alt": "Image",
+            "tm_figure_title": "",
+            "tm_figure_description": "",
+            "href": "img/3/67.jpg",
+            "text": "View More"
+        },
+        {
+            "src": "img/3/68.jpg",
+            "alt": "Image",
+            "tm_figure_title": "",
+            "tm_figure_description": "",
+            "href": "img/3/68.jpg",
+            "text": "View More"
+        },
+        {
+            "src": "img/3/69.jpg",
+            "alt": "Image",
+            "tm_figure_title": "",
+            "tm_figure_description": "",
+            "href": "img/3/69.jpg",
+            "text": "View More"
+        },
+        {
+            "src": "img/3/70.jpg",
+            "alt": "Image",
+            "tm_figure_title": "",
+            "tm_figure_description": "",
+            "href": "img/3/70.jpg",
+            "text": "View More"
+        },
+        {
+            "src": "img/3/71.jpg",
+            "alt": "Image",
+            "tm_figure_title": "",
+            "tm_figure_description": "",
+            "href": "img/3/71.jpg",
+            "text": "View More"
+        },
+        {
+            "src": "img/3/72.jpg",
+            "alt": "Image",
+            "tm_figure_title": "",
+            "tm_figure_description": "",
+            "href": "img/3/72.jpg",
+            "text": "View More"
+        },
+        {
+            "src": "img/3/73.jpg",
+            "alt": "Image",
+            "tm_figure_title": "",
+            "tm_figure_description": "",
+            "href": "img/3/73.jpg",
+            "text": "View More"
+        },
+        {
+            "src": "img/3/74.jpg",
+            "alt": "Image",
+            "tm_figure_title": "",
+            "tm_figure_description": "",
+            "href": "img/3/74.jpg",
+            "text": "View More"
+        },
+        {
+            "src": "img/3/75.jpg",
+            "alt": "Image",
+            "tm_figure_title": "",
+            "tm_figure_description": "",
+            "href": "img/3/75.jpg",
+            "text": "View More"
+        },
+        {
+            "src": "img/3/76.jpg",
+            "alt": "Image",
+            "tm_figure_title": "",
+            "tm_figure_description": "",
+            "href": "img/3/76.jpg",
+            "text": "View More"
+        },
+        {
+            "src": "img/3/77.jpg",
+            "alt": "Image",
+            "tm_figure_title": "",
+            "tm_figure_description": "",
+            "href": "img/3/77.jpg",
+            "text": "View More"
+        },
+        {
+            "src": "img/3/78.jpg",
+            "alt": "Image",
+            "tm_figure_title": "",
+            "tm_figure_description": "",
+            "href": "img/3/78.jpg",
+            "text": "View More"
+        },
+        {
+            "src": "img/3/79.jpg",
+            "alt": "Image",
+            "tm_figure_title": "",
+            "tm_figure_description": "",
+            "href": "img/3/79.jpg",
+            "text": "View More"
+        },
+        {
+            "src": "img/3/80.jpg",
+            "alt": "Image",
+            "tm_figure_title": "",
+            "tm_figure_description": "",
+            "href": "img/3/80.jpg",
+            "text": "View More"
+        },
+        {
+            "src": "img/3/81.jpg",
+            "alt": "Image",
+            "tm_figure_title": "",
+            "tm_figure_description": "",
+            "href": "img/3/81.jpg",
+            "text": "View More"
+        },
+        {
+            "src": "img/3/82.jpg",
+            "alt": "Image",
+            "tm_figure_title": "",
+            "tm_figure_description": "",
+            "href": "img/3/82.jpg",
+            "text": "View More"
+        },
+        {
+            "src": "img/3/83.jpg",
+            "alt": "Image",
+            "tm_figure_title": "",
+            "tm_figure_description": "",
+            "href": "img/3/83.jpg",
+            "text": "View More"
+        },
+        {
+            "src": "img/3/85.jpg",
+            "alt": "Image",
+            "tm_figure_title": "",
+            "tm_figure_description": "",
+            "href": "img/3/85.jpg",
+            "text": "View More"
+        },
+        {
+            "src": "img/3/87.jpg",
+            "alt": "Image",
+            "tm_figure_title": "",
+            "tm_figure_description": "",
+            "href": "img/3/87.jpg",
+            "text": "View More"
+        },
+        {
+            "src": "img/3/100.jpg",
+            "alt": "Image",
+            "tm_figure_title": "",
+            "tm_figure_description": "",
+            "href": "img/3/100.jpg",
+            "text": "View More"
+        }
+    ], // gallery three
+    [
+
+    ] // gallery four
+
+];
